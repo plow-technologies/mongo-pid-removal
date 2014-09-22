652f8b539d6f8630d772c500d402774f5f3d0e8c
brent80 <brent.phillips@plowtech.net>
2014-09-22 10:01:01 -0500

    Can connect to database

--- src/Mongo/Pid/Removal.hs
+++ src/Mongo/Pid/Removal.hs
@@ -1,3 +1,30 @@
-module Mongo.Pid.Removal where
+{- |
+Module      :  Mongo.Pid.Removal
+Description :  Get Pid from Name
+Copyright   :  (c) Plow Technology 2014
+License     :  MIT
 
-import Mongo.Pid.Removal.Internal
+Maintainer  :  brent.phillips@plowtech.net
+Stability   :  unstable
+Portability :  portable
+
+<Uses a name to grab the pid to remove old pid alarms from mongo>
+-}
+
+{-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
+
+module Mongo.Pid.Removal (removeMissingAlarms) where
+
+
+import BasicPrelude 
+import Persist.Mongo.Settings
+import Database.Persist
+import Data.Aeson
+
+removeMissingAlarms :: Either String MongoDBConf -> [Int] -> IO ()
+removeMissingAlarms (Left st) _ = putStrLn "Missing mongo config" >> print st
+removeMissingAlarms (Right mdbc) alarmListPids = do
+                          vals <- runDBConf mdbc $ do
+                            pids <- selectList [OnpingTagCombinedPid <-. (Just <$> alarmListPids)] []
+                            return pids
+                          print $ toJSON vals
